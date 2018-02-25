e5c24369cfb4f7b3567eac918fe285fd723ca119
Jenny Kam <jkam17@gmail.com>
2018-02-25 04:07:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,23 +21,8 @@ function sayHiToGrandma(x){
   else if (x==='HELLO'){
     return ('YES INDEED')
   }
-  else if (x===())
+  else if (x===('I love you, Grandma.')){
+    return('I love you, too')
+  }
 }
 
-/*
-
-
-describe('sayHiToGrandma(string)', function() {
-  it('returns "I can\'t hear you!" if `string` is lowercase', function() {
-    expect(sayHiToGrandma('hello')).toEqual("I can't hear you!")
-  })
-
-  it('returns "YES INDEED!" if `string` is uppercase', function() {
-    expect(sayHiToGrandma('HELLO')).toEqual("YES INDEED!")
-  })
-
-  it('returns "I love you, too." if `string` is "I love you, Grandma."`', function() {
-    expect(sayHiToGrandma("I love you, Grandma.")).toEqual("I love you, too.")
-  })
-})
-*/
